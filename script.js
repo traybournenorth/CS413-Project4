f71bab9782138275d36ae575007e87ad864377aa
[traybournenorth] <[tpn26@nau.edu]>
2019-11-30 15:38:16 -0700

    Updates

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-import * as PIXI from 'pixi.js';
-
 // The application will create a renderer using WebGL, if possible,
 // with a fallback to a canvas render. It will also setup the ticker
 // and the root stage PIXI.Container
-const app = new PIXI.Application();
+const app = new PIXI.Application({
+    width: 400,
+    height: 400
+});
 
 // The application will create a canvas element for you that you
 // can then insert into the DOM
