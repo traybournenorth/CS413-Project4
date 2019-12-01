b78e1c0fa688d377138cd280eab1442e23671a87
[traybournenorth] <[tpn26@nau.edu]>
2019-11-30 20:14:34 -0700

    Designed map

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
 const app = new PIXI.Application({
     width: 400,
     height: 400
 });
 
+// TileSize
 const tileSize = 16;
 
-// The application will create a canvas element for you that you
-// can then insert into the DOM
+// Creates world map
+let map = { width: 10, height: 10, 
+            tiles: [ 
+            // Dark top
+            12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
+            
+            // Door with ground underneath
+            12, 12, 35, 12, 12, 12, 12, 12, 50, 12,
+            0, 0, 35, 0, 0, 0, 0, 0, 0, 0,
+                
+            12, 12, 35, 12, 12, 12, 12, 12, 12, 12,
+            12, 12, 35, 12, 12, 12, 12, 12, 12, 12,
+            12, 12, 35, 12, 41, 12, 12, 36, 12, 12,
+            0, 0, 0, 0, 0, 0, 0, 36, 0, 0,
+            12, 12, 12, 12, 12, 12, 12, 36, 12, 12,
+            12, 12, 12, 12, 12, 12, 12, 36, 12, 12,
+                
+            // Bottom Floor
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            ]};
+
 document.body.appendChild(app.view);
 
 // load the texture we need
 app.loader.add('tileset', '/assets/bunny.png').load((loader, resources) => {
+    
     // Uncomment to play sound
     /*const sound = PIXI.sound.Sound.from('/assets/Blue.mp3');
     sound.volume = 0.5;
@@ -29,7 +49,8 @@ app.loader.add('tileset', '/assets/bunny.png').load((loader, resources) => {
         tileTextures[index] = new PIXI.Texture( resources.tileset.texture, new PIXI.Rectangle( x * tileSize, y * tileSize, tileSize, tileSize));
     }
     
-    const bunny = new PIXI.Sprite(tileTextures[53]);
+    // From top left to bottom right
+    const bunny = new PIXI.Sprite(tileTextures[49]);
 
     // Setup the position of the bunny
     bunny.x = app.renderer.width / 2;
@@ -38,13 +59,28 @@ app.loader.add('tileset', '/assets/bunny.png').load((loader, resources) => {
     // Rotate around the center
     bunny.anchor.x = 0.5;
     bunny.anchor.y = 0.5;
+    
+    let background = new PIXI.Container();
+    
+    for ( y = 0; y < map.width; y++ )
+    {
+        for ( let x = 0; x <map.width; x++ )
+        {
+            let tile = map.tiles[ y * map.width + x ];
+            let sprite = new PIXI.Sprite( tileTextures[ tile ] );
+            sprite.x = x * tileSize;
+            sprite.y = y * tileSize;
+            background.addChild( sprite );
+        }
+    }
+    
+    background.scale.x = 2.5;
+    background.scale.y = 2.5;
 
     // Add the bunny to the scene we are building
-    app.stage.addChild(bunny);
+    app.stage.addChild(background);
 
     // Listen for frame updates
     app.ticker.add(() => {
-         // each frame we spin the bunny around a bit
-        //bunny.rotation += 0.01;
     });
 });
