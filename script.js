c19b1b4992fd38732b6a778c74c58f8b1aec54c2
[traybournenorth] <[tpn26@nau.edu]>
2019-12-02 16:51:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -101,8 +101,80 @@ app.loader.load((loader, resources) => {
     startGame.on("mousedown", (event) => 
     {
         moveFlag = true;  
-        app.stage.removeChild( startGame );
-    })
+        startGame.visible = false;
+        instructions.visible = false;
+        credits.visible = false;
+    });
+    
+    let instructions = new PIXI.Text('Instructions',{fontFamily : 'Arial', fontSize: 24, fill : 0xFFFFFF });
+    instructions.position.x = 140;
+    instructions.position.y = 180;
+    
+    instructions.interactive = true;
+    instructions.buttonMode = true;
+    instructions.on("mousedown", (event) => 
+    {
+        startGame.visible = false;
+        instructions.visible = false;
+        credits.visible = false;
+        
+        let instructionsText = new PIXI.Text('Use the left, right, up, down arrow keys\nto make your way to each door. Try to\navoid incoming objects / obstacles that\ncould prevent you from winning. Good luck!',{fontFamily : 'Arial', fontSize: 18, fill : 0xFFFFFF });
+        instructionsText.position.x = 40;
+        instructionsText.position.y = 140;
+        
+        let backText = new PIXI.Text('Back to menu',{fontFamily : 'Arial', fontSize: 18, fill : 0xFFFFFF });
+        backText.position.x = 10;
+        backText.position.y = 370;
+    
+        backText.interactive = true;
+        backText.buttonMode = true;
+        backText.on("mousedown", (event) => 
+        {
+            instructionsText.visible = false;
+            backText.visible = false;
+            startGame.visible = true;
+            instructions.visible = true;
+            credits.visible = true;
+        });
+        
+        app.stage.addChild( instructionsText );
+        app.stage.addChild( backText );
+    });
+    
+    let credits = new PIXI.Text('Credits',{fontFamily : 'Arial', fontSize: 24, fill : 0xFFFFFF });
+    credits.position.x = 140;
+    credits.position.y = 220;
+    
+    credits.interactive = true;
+    credits.buttonMode = true;
+    credits.on("mousedown", (event) => 
+    {
+        startGame.visible = false;
+        instructions.visible = false;
+        credits.visible = false;
+        
+        let creditsText = new PIXI.Text('Game designers: Joseph Danciu, Jevin Dement, \nKeenan Swanson, Traybourne North\n\nEditors: Joseph Danciu, Jevin Dement, Keenan Swanson, \nTraybourne North\n\nProject Manager: Traybourne North\n\nComposer: Traybourne North',{fontFamily : 'Arial', fontSize: 14, fill : 0xFFFFFF });
+        creditsText.position.x = 20;
+        creditsText.position.y = 120;
+        
+        let backText = new PIXI.Text('Back to menu',{fontFamily : 'Arial', fontSize: 18, fill : 0xFFFFFF });
+        backText.position.x = 10;
+        backText.position.y = 370;
+    
+        backText.interactive = true;
+        backText.buttonMode = true;
+        backText.on("mousedown", (event) => 
+        {
+            creditsText.visible = false;
+            backText.visible = false;
+            startGame.visible = true;
+            instructions.visible = true;
+            credits.visible = true;
+        });
+        
+        app.stage.addChild( creditsText );
+        app.stage.addChild( backText );
+    });
     
     // Loop that slices size of image ( 7 by 11 ) into individual cubes
     for ( let index = 0; index < 7 * 11; index++ )
@@ -153,6 +225,8 @@ app.loader.load((loader, resources) => {
     
     // Add menu stage
     app.stage.addChild( startGame );
+    app.stage.addChild( instructions );
+    app.stage.addChild( credits );
     
     // Sets position of avatar
     //let character = { x: 0, y: 360, vx: 0, vy: 0 };
